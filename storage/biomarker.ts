17a163d584708592f8069196605a2ad9d8c3d453
N-Sam <nyekosamueltreg@gmail.com>
2026-08-03 16:45:51 +0300

    Feat: ExPO-SQLLITE KV storage test

--- storage/biomarker.ts
+++ storage/biomarker.ts
@@ -1,4 +1,5 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+//import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from 'expo-sqlite/kv-store';
 
 export type Biomarker = {
   id: string;
