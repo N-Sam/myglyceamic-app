18cfae95013ae25602daf774f084b150dbe44c1b
N-Sam <nyekosamueltreg@gmail.com>
2026-08-04 14:12:13 +0300

    Config: Added metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Add 'wasm' to the existing asset extensions
+config.resolver.assetExts.push('wasm');
+
+
 module.exports = config;
